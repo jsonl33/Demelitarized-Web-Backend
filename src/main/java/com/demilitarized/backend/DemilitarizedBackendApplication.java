ab5d3bca1203309ed61abbad11c7ba0cbe423a55
thisIsHyeongSeokLee <oddvapor@gmail.com>
2023-06-01 20:11:50 +0900

    basic project

---
+++ src/main/java/com/demilitarized/backend/DemilitarizedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demilitarized.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemilitarizedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemilitarizedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demilitarized/backend/DemilitarizedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demilitarized.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemilitarizedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
